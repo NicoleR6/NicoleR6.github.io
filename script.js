9159246c8a406714e61af8b6123d3bb5b0c91d19
NicoleR6 <nicole11022007@gmail.com>
2026-03-25 19:42:12 +0800

    Interaction: local 3D tilt on hover and removed main glass panel

--- script.js
+++ script.js
@@ -34,19 +34,21 @@ async function init() {
 
                 link.addEventListener('mousemove', (e) => {
                     const rect = link.getBoundingClientRect();
+                    
                     const x = e.clientX - rect.left;
                     const y = e.clientY - rect.top;
                     
-                    const rotateX = ((y - rect.height / 2) / rect.height) * -20;
-                    const rotateY = ((x - rect.width / 2) / rect.width) * 20;
+                    const centerX = rect.width / 2;
+                    const centerY = rect.height / 2;
+                    const rotateX = (-(y - centerY) / centerY) * 15;
+                    const rotateY = ((x - centerX) / centerX) * 15;
                     
-                    link.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(50px) translateY(-10px)`;
+                    link.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(40px) translateY(-5px)`;
                 });
 
                 link.addEventListener('mouseleave', () => {
                     link.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg) translateZ(0px) translateY(0px)`;
                 });
-                // ------------------------------
 
                 container.appendChild(link);
             });
